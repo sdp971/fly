231db77101201963d98849fba734557a2450a849
Séverine Dorimond Pereira <doris190485@hotmail.com>
2024-07-04 10:01:29 +0200

    functionality disabled questions added

--- src/components/Explication.jsx
+++ src/components/Explication.jsx
@@ -1,11 +1,21 @@
+import './Explication.css'
+import image from '../assets/image.png' 
+import { useEffect, useState } from 'react';
 
 
-function Explication  ({texte})  {
+function Explication({ texte }) {
+ const [showComponent, setShowComponent] = useState(false);
+
+ useEffect(() => {
+   setShowComponent(true);
+ }, []);
   return (
-    <div>
-     {texte}
+    // <div className={`explaination-container ${showComponent ? 'fade-in':"" }`}>
+    <div className={`explication-container ${showComponent ? 'show' : ''}`}>
+      <img src={image} alt='' className='explication-image' />
+      <p>{texte}</p>
     </div>
-  )
+  );
 }
 
 export default Explication
