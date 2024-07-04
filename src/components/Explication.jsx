b21a7a417d7cc5172e9a051a78db8d6fff2e37a2
Adeline <149504531+kna1703@users.noreply.github.com>
2024-07-04 12:19:38 +0200

    bouton suivant dans l'encart explication

--- src/components/Explication.jsx
+++ src/components/Explication.jsx
@@ -1,19 +1,24 @@
 import './Explication.css'
+import Button from "../components/Button"
 import image from '../assets/image.png' 
 import { useEffect, useState } from 'react';
 
 
-function Explication({ texte }) {
+function Explication({ texte, onclick }) {
  const [showComponent, setShowComponent] = useState(false);
 
  useEffect(() => {
    setShowComponent(true);
  }, []);
+
   return (
     // <div className={`explaination-container ${showComponent ? 'fade-in':"" }`}>
     <div className={`explication-container ${showComponent ? 'show' : ''}`}>
       <img src={image} alt='' className='explication-image' />
       <p>{texte}</p>
+      <div className='btn-next'>
+        <Button onclick={onclick}>SUIVANT</Button>
+      </div>
     </div>
   );
 }
