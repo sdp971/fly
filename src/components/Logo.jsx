643efff8f914aca9ffb9f8eba34e2830987d40f7
Adeline <149504531+kna1703@users.noreply.github.com>
2024-07-04 20:04:36 +0200

    link logo, css

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import FLY from "../assets/images/Logo_FLY.png";
 import './Logo.css'
 
 function Logo() {
   return (
     <div>
+      <Link to="/">
       <img src={FLY} alt="Logo Femme Like You" className='FLY'/>
+      </Link>
     </div>
   )
 }
